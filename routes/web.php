28d94906c1be2171f76eeaf74554f02892ac1026
Devu Krishnan P <devukrishnanp@gmail.com>
2026-03-30 11:17:21 +0530

    Improve turf booking UI
    Co-authored-by: Devu Krishnan P <devukrishnanp@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookingController;
 
+Route::redirect('/', '/turf/1');
 Route::get('/turf/{id}', [BookingController::class, 'index'])->name('bookings.index');
 Route::post('/bookings', [BookingController::class, 'store'])->name('bookings.store');
